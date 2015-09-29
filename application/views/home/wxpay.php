0ce24c08a0c666e42950b902de2bbc269dd11bcf
MonkeyKing <MonkeyKing1024@gmail.com>
2015-09-29 19:01:58 +0800

    update

--- application/views/home/wxpay.php
+++ application/views/home/wxpay.php
@@ -17,6 +17,7 @@
 			'getBrandWCPayRequest',
 			<?php echo $jsApiParameters; ?>,
 			function(res){
+		    	alert('2');
 				WeixinJSBridge.log(res.err_msg);
 				alert(res.err_code+res.err_desc+res.err_msg);
 			}
@@ -33,38 +34,39 @@
 		    }
 		}else{
 		    jsApiCall();
+		    alert('1');
 		}
 	}
-	//获取共享地址
-	function editAddress()
-	{
-		WeixinJSBridge.invoke(
-			'editAddress',
-			<?php echo $editAddress; ?>,
-			function(res){
-				var value1 = res.proviceFirstStageName;
-				var value2 = res.addressCitySecondStageName;
-				var value3 = res.addressCountiesThirdStageName;
-				var value4 = res.addressDetailInfo;
-				var tel = res.telNumber;
+	// //获取共享地址
+	// function editAddress()
+	// {
+	// 	WeixinJSBridge.invoke(
+	// 		'editAddress',
+	// 		<?php echo $editAddress; ?>,
+	// 		function(res){
+	// 			var value1 = res.proviceFirstStageName;
+	// 			var value2 = res.addressCitySecondStageName;
+	// 			var value3 = res.addressCountiesThirdStageName;
+	// 			var value4 = res.addressDetailInfo;
+	// 			var tel = res.telNumber;
 				
-				alert(value1 + value2 + value3 + value4 + ":" + tel);
-			}
-		);
-	}
+	// 			alert(value1 + value2 + value3 + value4 + ":" + tel);
+	// 		}
+	// 	);
+	// }
 	
-	window.onload = function(){
-		if (typeof WeixinJSBridge == "undefined"){
-		    if( document.addEventListener ){
-		        document.addEventListener('WeixinJSBridgeReady', editAddress, false);
-		    }else if (document.attachEvent){
-		        document.attachEvent('WeixinJSBridgeReady', editAddress); 
-		        document.attachEvent('onWeixinJSBridgeReady', editAddress);
-		    }
-		}else{
-			editAddress();
-		}
-	};
+	// window.onload = function(){
+	// 	if (typeof WeixinJSBridge == "undefined"){
+	// 	    if( document.addEventListener ){
+	// 	        document.addEventListener('WeixinJSBridgeReady', editAddress, false);
+	// 	    }else if (document.attachEvent){
+	// 	        document.attachEvent('WeixinJSBridgeReady', editAddress); 
+	// 	        document.attachEvent('onWeixinJSBridgeReady', editAddress);
+	// 	    }
+	// 	}else{
+	// 		editAddress();
+	// 	}
+	// };
 	
 </script>
 <font color="#9ACD32"><b>该笔订单支付金额为<span style="color:#f00;font-size:50px"><?php echo $fee;?>分</span>钱</b></font><br/><br/>
