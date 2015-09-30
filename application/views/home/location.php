f079ec5cd0658f94a9b365128439e2e017772442
MonkeyKing <MonkeyKing1024@gmail.com>
2015-09-30 20:48:37 +0800

    update

--- application/views/home/location.php
+++ application/views/home/location.php
@@ -1,26 +1,43 @@
-<!DOCTYPE html>
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
     <style type="text/css">
-        body, html {width: 100%;height: 100%;margin:0;font-family:"微软雅黑";}
-        #allmap{width:100%;height:500px;}
-        p{margin-left:5px; font-size:14px;}
+        body, html,#allmap {width: 100%;height: 100%;overflow: hidden;margin:0;font-family:"微软雅黑";}
     </style>
-    <script type="text/javascript" src="http://api.map.baidu.com/api?v=2.0&ak=您的密钥"></script>
-    <title>移动地图</title>
+    <script type="text/javascript" src="http://api.map.baidu.com/api?v=2.0&ak=N9eiFDikvcAKMBa3cCUxcNwH"></script>
+    <title>浏览器定位</title>
 </head>
 <body>
     <div id="allmap"></div>
-    <p>初始化地图，中心点为（116.4035,39.915），缩放级别为8的北京市地图，2秒后，通过改变中心点坐标，地图平移到广州</p>
 </body>
 </html>
 <script type="text/javascript">
     // 百度地图API功能
-    var map = new BMap.Map("allmap");       
-    map.centerAndZoom(new BMap.Point(116.4035,39.915),8); 
-    setTimeout(function(){
-        map.panTo(new BMap.Point(113.262232,23.154345));   //两秒后移动到广州
-    }, 2000);
+    var map = new BMap.Map("allmap");
+    var point = new BMap.Point(116.331398,39.897445);
+    map.centerAndZoom(point,12);
+
+    var geolocation = new BMap.Geolocation();
+    geolocation.getCurrentPosition(function(r){
+        if(this.getStatus() == BMAP_STATUS_SUCCESS){
+            var mk = new BMap.Marker(r.point);
+            map.addOverlay(mk);
+            map.panTo(r.point);
+            alert('您的位置：'+r.point.lng+','+r.point.lat);
+        }
+        else {
+            alert('failed'+this.getStatus());
+        }        
+    },{enableHighAccuracy: true})
+    //关于状态码
+    //BMAP_STATUS_SUCCESS   检索成功。对应数值“0”。
+    //BMAP_STATUS_CITY_LIST 城市列表。对应数值“1”。
+    //BMAP_STATUS_UNKNOWN_LOCATION  位置结果未知。对应数值“2”。
+    //BMAP_STATUS_UNKNOWN_ROUTE 导航结果未知。对应数值“3”。
+    //BMAP_STATUS_INVALID_KEY   非法密钥。对应数值“4”。
+    //BMAP_STATUS_INVALID_REQUEST   非法请求。对应数值“5”。
+    //BMAP_STATUS_PERMISSION_DENIED 没有权限。对应数值“6”。(自 1.1 新增)
+    //BMAP_STATUS_SERVICE_UNAVAILABLE   服务不可用。对应数值“7”。(自 1.1 新增)
+    //BMAP_STATUS_TIMEOUT   超时。对应数值“8”。(自 1.1 新增)
 </script>
