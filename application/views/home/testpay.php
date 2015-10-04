12bbb9ee388bd12c17fc5c7f9d38749a92f66912
MonkeyKing <MonkeyKing1024@gmail.com>
2015-10-04 22:21:59 +0800

    update

--- application/views/home/testpay.php
+++ application/views/home/testpay.php
@@ -32,36 +32,36 @@
 	}
 	</script>
 	<script type="text/javascript">
-	//获取共享地址
-	function editAddress()
-	{
-		WeixinJSBridge.invoke(
-			'editAddress',
-			<?php echo $editAddress; ?>,
-			function(res){
-				var value1 = res.proviceFirstStageName;
-				var value2 = res.addressCitySecondStageName;
-				var value3 = res.addressCountiesThirdStageName;
-				var value4 = res.addressDetailInfo;
-				var tel = res.telNumber;
+	// //获取共享地址
+	// function editAddress()
+	// {
+	// 	WeixinJSBridge.invoke(
+	// 		'editAddress',
+	// 		<?php echo $editAddress; ?>,
+	// 		function(res){
+	// 			var value1 = res.proviceFirstStageName;
+	// 			var value2 = res.addressCitySecondStageName;
+	// 			var value3 = res.addressCountiesThirdStageName;
+	// 			var value4 = res.addressDetailInfo;
+	// 			var tel = res.telNumber;
 				
-				alert(value1 + value2 + value3 + value4 + ":" + tel);
-			}
-		);
-	}
+	// 			alert(value1 + value2 + value3 + value4 + ":" + tel);
+	// 		}
+	// 	);
+	// }
 	
-	window.onload = function(){
-		if (typeof WeixinJSBridge == "undefined"){
-		    if( document.addEventListener ){
-		        document.addEventListener('WeixinJSBridgeReady', editAddress, false);
-		    }else if (document.attachEvent){
-		        document.attachEvent('WeixinJSBridgeReady', editAddress); 
-		        document.attachEvent('onWeixinJSBridgeReady', editAddress);
-		    }
-		}else{
-			editAddress();
-		}
-	};
+	// window.onload = function(){
+	// 	if (typeof WeixinJSBridge == "undefined"){
+	// 	    if( document.addEventListener ){
+	// 	        document.addEventListener('WeixinJSBridgeReady', editAddress, false);
+	// 	    }else if (document.attachEvent){
+	// 	        document.attachEvent('WeixinJSBridgeReady', editAddress); 
+	// 	        document.attachEvent('onWeixinJSBridgeReady', editAddress);
+	// 	    }
+	// 	}else{
+	// 		editAddress();
+	// 	}
+	// };
 	
 	</script>
 </head>
