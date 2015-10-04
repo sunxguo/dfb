560f89d08980078bbe99e41c9e5ef8272caf55ba
MonkeyKing <MonkeyKing1024@gmail.com>
2015-10-05 00:23:27 +0800

    update

--- application/helpers/wxpay_helper.php
+++ application/helpers/wxpay_helper.php
@@ -818,7 +818,7 @@ class JsApi_pub extends Common_util_pub
         //初始化curl
        	$ch = curl_init();
 		//设置超时
-		curl_setopt($ch, CURLOP_TIMEOUT, $this->curl_timeout);
+//		curl_setopt($ch, CURLOP_TIMEOUT, $this->curl_timeout);
 		curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,FALSE);
         curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,FALSE);
