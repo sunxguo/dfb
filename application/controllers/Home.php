2d13e4874956422bf1ea825b15e8b0e3bca56077
MonkeyKing <MonkeyKing1024@gmail.com>
2015-10-04 21:17:33 +0800

    update

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -10,13 +10,48 @@ class Home extends CI_Controller {
 	}
 
 	public function index(){
-		$data=array();
-		//$this->load->view('/home/index',$data);
-		if(isset($_SESSION['userid'])){
-			$this->load->view('/home/index',$data);
-		}else{
-			$this->load->view('/home/login',$data);
-		}
+		// $data=array();
+		// //$this->load->view('/home/index',$data);
+		// if(isset($_SESSION['userid'])){
+		// 	$this->load->view('/home/index',$data);
+		// }else{
+		// 	$this->load->view('/home/login',$data);
+		// }
+		$this->load->library('WxPayApi');
+		$this->load->library('JsApiPay');
+		//①、获取用户openid
+		$tools = $this->jsapipay;
+		$openId = $tools->GetOpenid();
+
+		//②、统一下单
+		$input = new WxPayUnifiedOrder();
+		$input->SetBody("test");
+		$input->SetAttach("test");
+		$input->SetOut_trade_no(201500600000000000000);
+		$input->SetTotal_fee(1);
+		$input->SetTime_start(date("YmdHis"));
+		$input->SetTime_expire(date("YmdHis", time() + 600));
+		$input->SetGoods_tag("test");
+		$input->SetNotify_url("http://dfb.fengdukeji.com/common/wxpaynotify");
+		$input->SetTrade_type("JSAPI");
+		$input->SetOpenid($openId);
+		$order = $this->wxpayapi->unifiedOrder($input);
+		// echo '<font color="#f00"><b>统一下单支付单信息</b></font><br/>';
+		// print_r($order);
+		$jsApiParameters = $tools->GetJsApiParameters($order);
+
+		//获取共享收货地址js函数参数
+		$editAddress = $tools->GetEditAddressParameters();
+
+		//③、在支持成功回调通知中处理成功之后的事宜，见 notify.php
+		/**
+		 * 注意：
+		 * 1、当你的回调地址不可访问的时候，回调通知会失败，可以通过查询订单来确认支付是否成功
+		 * 2、jsapi支付时需要填入用户openid，WxPay.JsApiPay.php中有获取openid流程 （文档可以参考微信公众平台“网页授权接口”，
+		 * 参考http://mp.weixin.qq.com/wiki/17/c0f37d5704f0b64713d5d2c37b468d75.html）
+		 */
+		$data=array('jsApiParameters'=>$jsApiParameters,'fee'=>1);
+		$this->load->view('/home/wxpay',$data);
 	}
 	public function login(){
 		$data=array();
@@ -58,6 +93,9 @@ class Home extends CI_Controller {
 		}
 		$this->wxpay($_SESSION['number'],$_SESSION['fee']);
 		//$this->wxpay($info['number']);
+	}
+	public function testpay(){
+
 	}
 	public function wxpay($number,$fee){
 		$this->load->library('WxPayApi');
